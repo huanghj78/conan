e09f896e001d69eaa6e60049e915bdbf9be45c4a
huanghj78 <1353188493@qq.com>
2024-04-08 20:04:51 +0800

    init

--- systems/etcd/setup.sh
+++ systems/etcd/setup.sh
@@ -3,9 +3,9 @@ docker-compose up -d
 
 
 {
-  docker cp /root/cpfi-v2/scripts/chaosblade-1.3.0 bin-node1-1:/ &
-  docker cp /root/cpfi-v2/scripts/chaosblade-1.3.0 bin-node2-1:/ &
-  docker cp /root/cpfi-v2/scripts/chaosblade-1.3.0 bin-node3-1:/ &
+  docker cp ../../scripts/chaosblade-1.3.0 bin-node1-1:/ &
+  docker cp ../../scripts/chaosblade-1.3.0 bin-node2-1:/ &
+  docker cp ../../scripts/chaosblade-1.3.0 bin-node3-1:/ &
   wait
 }
 
--- systems/rqlite/setup.sh
+++ systems/rqlite/setup.sh
@@ -1,11 +1,11 @@
-docker-compose -f /root/cpfi-v2/systems/rqlite/docker-compose.yaml down -v
-docker-compose -f /root/cpfi-v2/systems/rqlite/docker-compose.yaml up -d
+docker-compose  down -v
+docker-compose  up -d
 
 
 {
-  docker cp /root/cpfi-v2/scripts/chaosblade-1.3.0 rqlite-node1-1:/ &
-  docker cp /root/cpfi-v2/scripts/chaosblade-1.3.0 rqlite-node2-1:/ &
-  docker cp /root/cpfi-v2/scripts/chaosblade-1.3.0 rqlite-node3-1:/ &
+  docker cp ../../scripts/chaosblade-1.3.0 rqlite-node1-1:/ &
+  docker cp ../../scripts/chaosblade-1.3.0 rqlite-node2-1:/ &
+  docker cp ../../scripts/chaosblade-1.3.0 rqlite-node3-1:/ &
   wait
 }
 
