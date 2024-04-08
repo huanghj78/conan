66b63cf36359b00534b3ee53f61b28a7e0d814d2
huanghj78 <1353188493@qq.com>
2024-04-08 19:17:28 +0800

    init

--- systems/opengauss/workload/cal.sh
+++
@@ -1,3 +0,0 @@
-# docker exec node1 env LD_LIBRARY_PATH=/home/opengauss/openGauss/install/lib /home/opengauss/openGauss/install/bin/gsql -p 21000 -d postgres -r -c "CREATE TABLE t (id INT);"
-
-time docker exec node1 env LD_LIBRARY_PATH=/home/opengauss/openGauss/install/lib /home/opengauss/openGauss/install/bin/gsql -p 21000 -d postgres -r -c "EXPLAIN INSERT INTO t VALUES (1);"
--- systems/rqlite/workload/cal.sh
+++
@@ -1,3 +0,0 @@
-time curl -XPOST '172.16.237.100:2379/db/execute?pretty&timings' -H "Content-Type: application/json" -d "[
-    \"INSERT INTO t(id) VALUES('1')\"
-]"
